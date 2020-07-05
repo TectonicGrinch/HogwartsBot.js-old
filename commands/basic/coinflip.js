4afc68eb1e4a7f63269923cc54ce2703e688fb6d
JuicyShark <maxwellb9879@gmail.com>
2020-07-05 16:38:19 +1000

    layout and syntax fix

--- commands/basic/coinflip.js
+++ commands/basic/coinflip.js
@@ -1,35 +1,35 @@
 const commando = require('discord.js-commando')
 
 class coinflip extends commando.Command {
-        constructor(client) {
-            super(client, {
-                name: 'coinflip',
-                aliases: ['cf'],
-                group: 'basic',
-                memberName: 'coinflip',
-                description: 'Flip a coin 50/50 chance.',
-                counter: {
-                    Heads: 0,
-                    Tails: 0
-                }
-            });
-        }
-        async run(message, args) {
-            var msg = flip()
-            message.reply(msg);
-        }
-
-        function flip() {
-            var flip = Math.floor(Math.random() * 100) + 1;
-            var currentFlip = null;
-            if (flip >= 50) {
-                currentflip == 'Heads'
-                coinflip.counter[currentflip]++
-            } else {
-                currentflip == 'Tails'
-                coinflip.counter[currentflip]++
+    constructor(client) {
+        super(client, {
+            name: 'coinflip',
+            aliases: ['cf'],
+            group: 'basic',
+            memberName: 'coinflip',
+            description: 'Flip a coin 50/50 chance.',
+            counter: {
+                Heads: 0,
+                Tails: 0
             }
-            return `<:galleon:727038073678069811>*flips a coin and it lands on* **${currentflip}** ${coinflip.counter[currentflip]}`
-        }
+        });
+    }
+    async run(message, args) {
+        message.reply(flip());
+    }
+}
 
-        module.exports = coinflip;
+function flip() {
+    var roll = Math.floor(Math.random() * 100) + 1;
+    var currentflip = null;
+    if (roll >= 50) {
+        currentflip = 'Heads'
+        this.counter[currentflip]++
+    } else {
+        currentflip = 'Tails'
+        this.counter[currentflip]++
+    }
+    return `<:galleon:727038073678069811> *flips a coin and it lands on* **${currentflip}**, ${currentflip}, has been flipped a total of ${this.counter[currentflip]} times`
+}
+
+module.exports = coinflip;
